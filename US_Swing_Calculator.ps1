b87d3783b3b60285fd8e6655db028b8e51b2da2c
unknown <guycton@gmail.com>
2020-11-05 02:23:28 +0200

    US Swing Calculator added

---
+++ US_Swing_Calculator.ps1
@@ -0,0 +1,18 @@
+﻿$stockToday = 113
+$stockTarget = 112
+$totalILSB = 5000
+$Dollar = 3.5
+$commissionExchange = 0.01
+$commissionTrade = 8
+
+$DollarIncomeP = $Dollar + $commissionExchange
+$DollarOutcomeP = $Dollar - $commissionExchange
+
+$totalDollarsB = ($totalILSB / $DollarIncomeP) - $commissionTrade
+$precentOfProfit = $stockTarget / $stockToday
+$totalDollarsA = $totalDollarsB * $precentOfProfit
+
+$totalILSA = ($totalDollarsA * $DollarOutcomeP) - $commissionTrade
+
+
+Write-Host "$totalILSA"
